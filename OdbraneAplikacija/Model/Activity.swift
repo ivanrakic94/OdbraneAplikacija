eed0334facca85bbd63ecca2ecb407e082eec8f0
Željana Milošević <zeljanamilosevic@Zeljanas-iMac.local>
2017-12-21 14:34:51 +0100

    Ispravljena greska kod klasa Activity i UpdateViewController.

---
+++ OdbraneAplikacija/Model/Activity.swift
@@ -0,0 +1,49 @@
+//
+//  Activity.swift
+//  OdbraneAplikacija
+//
+//  Created by Željana Milošević on 12/21/17.
+//  Copyright © 2017 Ivan Rakić. All rights reserved.
+//
+
+import UIKit
+
+class Activity {
+    
+    var ActivityID : Int32 {
+        get {
+            return self.ActivityID
+        }
+        set (ActivityID) {
+            self.ActivityID = ActivityID
+        }
+    }
+    
+    var ActivityName : String {
+        get {
+            return self.ActivityName
+        }
+        set (ActivityName) {
+            self.ActivityName = ActivityName
+        }
+    }
+    
+    var TopicList : Array<Topic> = Array() {
+        get {
+            return self.TopicList
+        }
+        set (TopicListP) {
+            self.TopicList = TopicListP
+        }
+    }
+    
+    var TemplateList : Array<Template> = Array() {
+        get {
+            return self.TemplateList
+        }
+        set (TemplateListP) {
+            self.TemplateList = TemplateListP
+        }
+    }
+}
+
